a57012ad06253bc4872559f80f1bbaa76df98f23
Rodrigo <a85280@ualg.pt>
2023-12-03 16:28:00 +0000

    Adicionei função que imprime uma lista de vieculos à aplicação

--- src/veiculos.py
+++ src/veiculos.py
@@ -17,4 +17,9 @@ def cria_novo_veiculo():
     veiculo = {"marca": marca,
                "matricula": matricula}
 
-    return veiculo
+    retunr veiculo
+
+def imprime_lista_de_veiculos(lista_de_veiculos):
+    """TODO: documentação"""
+
+    imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
